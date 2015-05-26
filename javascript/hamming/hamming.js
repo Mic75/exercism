2ab2c8472572dae1a384925bcb2ea7a0d34f3a6e
Mic75 <mat.benard@gmail.com>
2015-05-25 22:47:45 -0400

    Submit hamming

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,16 @@
+﻿var Hamming = function () { };
+Hamming.prototype.compute = function (strand1, strand2) {
+    var strand_length = strand1.length;
+    if (strand_length !== strand2.length) {
+        throw new Error("DNA strands must be of equal length.");
+    }
+
+    var distance = 0;
+    for (var i = 0; i < strand_length ; i++) {
+        if (strand1.charAt(i) !== strand2.charAt(i)) {
+            distance++;
+        }
+    }
+    return distance;
+};
+module.exports = Hamming.prototype;
