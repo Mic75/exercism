46e437dbb291ebcca1e1b5cf60513dec2d988e1a
Mic75 <mat.benard@gmail.com>
2015-07-02 16:46:49 -0400

    Submit rna-transcription

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,16 @@
+﻿var transcriptor = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+};
+
+function toRNA(dna) {
+    var rna_strand = "", dna_length;
+    for (i = 0, dna_length = dna.length; i < dna_length ; i++) {
+        rna_strand += transcriptor[dna[i]];
+    }
+    return rna_strand;
+}
+
+module.exports = toRNA;
