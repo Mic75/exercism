bf856ee16cd11d2befddaf83fdc083b4b53c088f
Mic75 <mat.benard@gmail.com>
2015-07-03 16:45:43 -0400

    Grade school in progress, add insertion sort to make the test pass

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,29 @@
+﻿var School = function () {
+    this.school_roster = {};
+};
+
+School.prototype = {
+    roster: function () {
+        return this.school_roster;
+    },
+    add: function (student, grade) {
+        students_in_grade = this.school_roster[grade];
+        if (typeof students_in_grade === "undefined") {
+            this.school_roster[grade] = [student];
+        }
+        else {
+            this.school_roster[grade].push(student);
+        }
+    },
+    grade: function (grade) {
+        var enrolled = this.school_roster[grade];
+        if (typeof enrolled !== "undefined" ){
+            return enrolled.sort();
+        }
+        else {
+            return [];
+        }
+    }
+};
+
+module.exports = School;
