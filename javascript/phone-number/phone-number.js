4e7ac9c40e8b2ca21f2e64989e230302a949c331
Mic75 <mat.benard@gmail.com>
2015-07-03 14:01:05 -0400

    Phone-number itaration 2, a little more concise

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,37 +1,39 @@
-﻿function clean_number(number) {
+﻿PhoneNumber = function (number) {
 
-    var cleaned_num = number.replace(/[\(\)\.\s-]/g, "");
-    var wrong_number = "0000000000";
+    function clean_number(number) {
+        var cleaned_num = number.replace(/[\(\)\.\s-]/g, "");
+        var wrong_number = "0000000000";
 
-    if (cleaned_num.length < 10 || cleaned_num.length > 11) {
-        return wrong_number;
-    }
-
-    if (cleaned_num.length === 11) {
-        if (cleaned_num[0] === "1") {
-            return cleaned_num.substr(1);
-        }
-        else {
+        if (cleaned_num.length < 10 || cleaned_num.length > 11) {
             return wrong_number;
         }
-    }
-    return cleaned_num;
-}
 
-PhoneNumber = function (number) {
+        if (cleaned_num.length === 11) {
+            if (cleaned_num[0] === "1") {
+                return cleaned_num.substr(1);
+            }
+            else {
+                return wrong_number;
+            }
+        }
+        return cleaned_num;
+    }
+
     this.phone_number = clean_number(number);
 };
 
-PhoneNumber.prototype.number = function () {
-    return this.phone_number;
+PhoneNumber.prototype = {
+    number: function () {
+        return this.phone_number;
+    },
+
+    areaCode: function () {
+        return this.phone_number.substr(0, 3);
+    },
+
+    toString: function () {
+        return this.phone_number.replace(/(^\d{3})(\d{3})(\d{4})/g, "($1) $2-$3");
+    }
 };
-
-PhoneNumber.prototype.areaCode = function () {
-    return this.phone_number.substr(0, 3);
-}
-
-PhoneNumber.prototype.toString = function () {
-   return  this.phone_number.replace(/(^\d{3})(\d{3})(\d{4})/g, "($1) $2-$3");
-}
 
 module.exports = PhoneNumber;
