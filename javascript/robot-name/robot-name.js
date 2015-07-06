201ebaecc19eff2589aacb87f5707767f171da37
Mic75 <mat.benard@gmail.com>
2015-07-06 16:05:35 -0400

    Submit robot-name

---
+++ javascript/robot-name/robot-name.js
@@ -0,0 +1,29 @@
+﻿module.exports = (function () {
+
+    function get_random_int(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function get_bigram() {
+        return String.fromCharCode(get_random_int(65, 91),get_random_int(65, 91));
+    }
+
+    function get_serial() {
+        return new Intl.NumberFormat('en-US', { minimumIntegerDigits: 3 }).format(get_random_int(0, 1000));
+    }
+
+    function get_new_name() {
+        return get_bigram() + get_serial();
+    }
+
+    var Robot = function () {
+        this.name = get_new_name();
+    };
+
+    Robot.prototype = {
+        reset: function () {
+            this.name = get_new_name();
+        }
+    };
+    return Robot;
+})();
