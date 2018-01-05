ef6de39d5b5139e1b87ea5b462b7b2215451be96
Mic <mat.benard@gmail.com>
2018-01-05 16:37:21 -0500

    ecmascript:bob: tests ok

---
+++ ecmascript/bob/bob.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+
+
+class Bob {
+  hey(message) {
+
+    message = message.trim();
+
+    if (message.length === 0){
+      return 'Fine. Be that way!'
+    }
+
+    if (/[a-z\xfc\xe4]/.test(message) === false && /[A-Z\xc4]+/.test(message) === true) { // Expect only capitalized char
+      return 'Whoa, chill out!';
+    }
+
+    if (/\w+\?$/.test(message)) { // Except question mark at the end
+      return 'Sure.';
+    }
+
+    return 'Whatever.';
+  }
+}
+
+export default Bob;
+
