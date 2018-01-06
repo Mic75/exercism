fd36dbecf8273bf3c349e0abe5d6b18415bcc75b
Mic <mat.benard@gmail.com>
2018-01-05 19:07:04 -0500

    ecmascript:prime-factors: iteration 2

--- ecmascript/prime-factors/prime-factors.js
+++ ecmascript/prime-factors/prime-factors.js
@@ -1,25 +1,14 @@
-
-function isPrime(value){
-  let divisor = value - 1;
-  while (value%divisor !== 0){
-    divisor--;
-  }
-  return divisor === 1;
-}
-
 class PrimeFactors {
   for(value) {
     let dividend = value;
     let factors = [];
     let currentDivisor = 2;
-    while (dividend !== 1){
-      if (dividend%currentDivisor === 0){
-        if (isPrime(currentDivisor)) {
-          factors.push(currentDivisor);
-        }
-        dividend = dividend / currentDivisor;
+    while (dividend !== 1) {
+      if (dividend % currentDivisor === 0) {
+        factors.push(currentDivisor);
+        dividend /= currentDivisor;
       } else {
-        currentDivisor += 1;
+        currentDivisor++;
       }
     }
     return factors;
