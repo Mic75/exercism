84f16a53a04910308aba2ea436832f28abe4d244
Mic <mat.benard@gmail.com>
2018-10-28 21:30:54 -0400

    js:wordy: basic tests passing

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -3,19 +3,28 @@ class ArgumentError extends Error {}
 class WordProblem {
   constructor(question){
     this.question = question;
-    this._operationsMapping = new Map([
-      ['plus', '_add'],
-      ['minus', '_subtract']
-    ]);
+    this._expressionRegEx = /(-?\d+) (plus|minus|multiplied by|divided by) (-?\d+)/;
   }
 
   answer() {
-    let matches = this.question.match(/(\d+) (plus|minus) (\d+)/);
-    return this[this._operationsMapping.get(matches[2])](parseInt(matches[1]), parseInt(matches[3]));
+    let matches = this.question.match(this._expressionRegEx);
+    return this[`_${matches[2]}`](parseInt(matches[1]), parseInt(matches[3]));
   }
 
-  _add(a,b) {
+  _plus(a,b) {
     return a + b;
+  }
+
+  _minus(a,b) {
+    return a - b;
+  }
+
+  ["_multiplied by"](a,b) {
+    return a * b;
+  }
+
+  ["_divided by"](a,b) {
+    return a / b;
   }
 }
 
