94eb8dfea580d701b69147fd4b1838356f5d609e
Mathieu Benard <Mic75@users.noreply.github.com>
2019-12-04 11:15:10 -0500

    js:rna-transcription: applies mentor comment

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,7 +1,14 @@
 const RNA_TO_DNA = { G: 'C', C: 'G', T: 'A', A: 'U' };
 
+const tryTranscribeNucleotid = nucleotid => {
+  const transcribedNucleotid = RNA_TO_DNA[nucleotid];
+  if(!transcribedNucleotid)
+    throw new RangeError(`Unknown nucleotid ${nucleotid}.`);
+  return transcribedNucleotid;
+};
+
 export function toRna(strand) {
-  return strand.split("")
-    .map(nucleotide => RNA_TO_DNA[nucleotide])
+  return [...strand]
+    .map(tryTranscribeNucleotid)
     .join('');
 }
