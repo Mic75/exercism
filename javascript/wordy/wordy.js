8e53ec8caef16bc4a357be9b5afa40059aa7e80a
Mic <mat.benard@gmail.com>
2019-12-11 19:56:54 -0500

    javascript:wordy: detach unvarying fields from instance

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -3,26 +3,16 @@ class ArgumentError extends Error {}
 class WordProblem {
   constructor(question){
     this.question = question;
-    this._operandsRegEx = /-?\d+/g;
-    this._operations = {
-      "plus": (a,b) => a + b,
-      "minus": (a, b) => a - b,
-      "multiplied by": (a,b) => a*b,
-      "divided by": (a,b) => a/b
-    };
-
-    this._operators = /(plus|minus|multiplied by|divided by)/g;
-    this._unsupportedOperators = /(cubed)/g;
   }
 
   answer() {
-    let operands = this.question.match(this._operandsRegEx) || [];
+    let operands = this.question.match(_operandsRegEx) || [];
     operands = operands.map(o => parseInt(o));
 
-    const operators = this.question.match(this._operators) || [];
-    const unsupportedOperators = this.question.match(this._unsupportedOperators);
+    const operators = this.question.match(_operators) || [];
+    const unsupportedOperators = this.question.match(_unsupportedOperators);
 
-    if (operators.length === 0 && operands.length === 1 && this._unsupportedOperators === null){
+    if (operators.length === 0 && operands.length === 1 && _unsupportedOperators === null){
       return operands[0];
     }
 
@@ -30,13 +20,25 @@ class WordProblem {
       throw new ArgumentError();
     }
 
-    let result = this._operations[operators[0]](operands[0], operands[1]);
+    let result = _operations[operators[0]](operands[0], operands[1]);
 
     for (let i=2 ; i < operands.length; i++){
-      result = this._operations[operators[i-1]](result, operands[i]);
+      result = _operations[operators[i-1]](result, operands[i]);
     }
     return result;
   }
 }
+
+const _operandsRegEx = /-?\d+/g;
+
+const _operations = {
+  "plus": (a,b) => a + b,
+  "minus": (a, b) => a - b,
+  "multiplied by": (a,b) => a*b,
+  "divided by": (a,b) => a/b
+};
+const _operators = /(plus|minus|multiplied by|divided by)/g;
+
+const _unsupportedOperators = /(cubed)/g;
 
 export { ArgumentError, WordProblem }
