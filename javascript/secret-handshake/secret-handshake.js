e4c9783e35093033f27bdef15632f658525fd9c7
Mic <mat.benard@gmail.com>
2019-12-11 19:49:22 -0500

    Update solutions

---
+++ javascript/secret-handshake/secret-handshake.js
@@ -0,0 +1,23 @@
+export function secretHandshake(secret) {
+
+  if (typeof secret !== 'number') {
+    throw Error('Handshake must be a number');
+  }
+
+  let cmd = [];
+
+  let secretMap = new Map([
+    [1, () => this.cmd.push('wink')],
+    [2, () => this.cmd.push('double blink')],
+    [4, () => this.cmd.push('close your eyes')],
+    [8, () => this.cmd.push('jump')],
+    [16, () => this.cmd.reverse()],
+  ]);
+
+  for (let [key, fnc] of secretMap) {
+    if ((key & secret) === key) {
+      fnc();
+    }
+  }
+  return cmd;
+}
