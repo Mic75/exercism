e1f05728ecab43d92b3b38d2e006ff5a8899b194
Mathieu Benard <Mic75@users.noreply.github.com>
2019-12-17 12:37:42 -0500

    js:anagram: done

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,9 +1,34 @@
 export class Anagram {
     constructor(input){
-
+        this.subject = input.toLowerCase();
+        this.subjectChars = [...this.subject];
     }
 
-    match(candidates){
+    matches(candidates){
+        return candidates.reduce((anagrams, candidate) => {
+            if (this._isAnagram(candidate)) {
+                anagrams.push(candidate);
+            }
+            return anagrams;
+        }, [])
+    }
 
+    _isAnagram(candidate) {
+        candidate = candidate.toLowerCase();
+        const candidateChars = [...candidate];
+        const remainingChars = this.subjectChars.slice();
+
+        if (candidate === this.subject)
+            return false;
+
+        for (const currentChar of candidateChars){
+            const indexInSubject = remainingChars.indexOf(currentChar);
+            if (indexInSubject === -1)
+                return false;
+
+            remainingChars.splice(indexInSubject, 1);
+        }
+
+        return candidateChars.length === this.subjectChars.length && remainingChars.length === 0;
     }
 }
