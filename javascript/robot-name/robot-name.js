92d47d434ccf3f99487ae557bbea7e5a82a466f7
Mic <mat.benard@gmail.com>
2020-01-03 17:14:05 -0500

    javascript:robot-name: simplifies char manipulation

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -4,12 +4,14 @@ class RobotFactory {
     this._names = new Array(RobotFactory.CAPACITY);
     this.cursor = 0;
     const lastNumberSequence = [0,2,4,6,8,1,3,5,7,9];
+    const alphabet =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
     let c = 0;
-    for (let i = 65; i < 91; i++) {
-      for (let j = 65; j < 91; j++) {
+    for (let i = 0, count = alphabet.length;  i < count; i++) {
+      for (let j = 0; j < count; j++) {
         for (let k = 0; k < 1000; k++) {
           let number = k.toString(10).padStart(3, "0").substr(0,2) + lastNumberSequence[k%10].toString(10);
-          this._names[c] = String.fromCharCode(i) + String.fromCharCode(j) + number;
+          this._names[c] = alphabet[i] + alphabet[j] + number;
           c++;
         }
       }
